8aed2cc4cf25a924672db123212ca4e208806d36
Stanley Wei Wen Ong <stanley.ong.wei.wen@gmail.com>
2023-06-15 20:21:15 +0800

    Add home route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/home", (req, res) => {
+  res.send("I am at homeeee!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
